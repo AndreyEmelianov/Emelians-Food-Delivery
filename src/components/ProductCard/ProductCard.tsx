a223dadacb070e7647dbb47abbe8955432856424
Andrey Emelianov <621077@mail.ru>
2023-09-06 16:11:58 +0300

    сверстал карточку товара, добавил стили

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,27 +1,30 @@
+import { Link } from 'react-router-dom';
 import styles from './ProductCard.module.css';
 import { IProductCardProps } from './ProductCard.props';
 
 function ProductCard({ id, title, description, image, price, rating }: IProductCardProps) {
 	return (
-		<div className={styles['card']}>
-			<div className={styles['head']}>
-				<div className={styles['price']}>
-					{price}
-					<span className={styles['currency']}>₽</span>
+		<Link to={'/'} className={styles['link']}>
+			<div className={styles['card']}>
+				<div className={styles['head']} style={{ backgroundImage: `url('${image}')` }}>
+					<div className={styles['price']}>
+						{price} &nbsp;
+						<span className={styles['currency']}>₽</span>
+					</div>
+					<button className={styles['add-to-cart']}>
+						<img src="/cart-button-icon.svg" alt="Добавить в корзину" />
+					</button>
+					<div className={styles['rating']}>
+						{rating} &nbsp;
+						<img src="/star-icon.svg" alt="Иконка рейтинга" />
+					</div>
 				</div>
-				<button className={styles['add-to-cart']}>
-					<img src="/cart-button-icon.svg" alt="Добавить в корзину" />
-				</button>
-				<div className={styles['rating']}>
-					{rating}
-					<img src="/star-icon.svg" alt="Иконка рейтинга" />
+				<div className={styles['footer']}>
+					<div className={styles['title']}>{title}</div>
+					<div className={styles['description']}>{description}</div>
 				</div>
 			</div>
-			<div className={styles['footer']}>
-				<div className={styles['title']}>{title}</div>
-				<div className={styles['description']}>{description}</div>
-			</div>
-		</div>
+		</Link>
 	);
 }
 
