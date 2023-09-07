7ce7aa75fd5bc037918820f7b2e9512c0c2434f7
Andrey Emelianov <621077@mail.ru>
2023-09-07 14:34:17 +0300

    добавил компоненту Login состояние ошибки, получение текста ошибки с бэкенда, реализовал просую функцию получения данных из полей формы и отправку их на бэкенд, при корректно введённом логине и пароле успешно приходит аксес токен

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,24 +4,57 @@ import Headling from '../../components/ui/Headling/Headling';
 import Input from '../../components/ui/Input/Input';
 
 import styles from './Login.module.css';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { PREFIX_URL } from '../../helpers/API';
+
+export type LoginForm = {
+	email: {
+		value: string;
+	};
+	password: {
+		value: string;
+	};
+};
 
 export function Login() {
-	const submit = (e: FormEvent) => {
+	const [error, setError] = useState<string | null>();
+
+	const submit = async (e: FormEvent) => {
 		e.preventDefault();
+		setError(null);
+		const target = e.target as typeof e.target & LoginForm;
+		const { email, password } = target;
+		await sendLogin(email.value, password.value);
+	};
+
+	const sendLogin = async (email: string, password: string) => {
+		try {
+			const { data } = await axios.post(`${PREFIX_URL}/auth/login`, {
+				email,
+				password,
+			});
+
+			console.log(data);
+		} catch (e) {
+			if (e instanceof AxiosError) {
+				setError(e.response?.data.message);
+			}
+		}
 	};
 
 	return (
-		<div className={styles['login']} onSubmit={submit}>
+		<div className={styles['login']}>
 			<Headling>Вход</Headling>
-			<form className={styles['form']}>
+			{error && <div className={styles['error']}>{error}</div>}
+			<form className={styles['form']} onSubmit={submit}>
 				<div className={styles['field']}>
 					<label htmlFor="email">Ваш email</label>
-					<Input id="email" placeholder="Email" />
+					<Input id="email" placeholder="Email" name="email" />
 				</div>
 				<div className={styles['field']}>
 					<label htmlFor="password">Ваш пароль</label>
-					<Input id="password" placeholder="Пароль" type="password" />
+					<Input id="password" placeholder="Пароль" type="password" name="password" />
 				</div>
 				<Button appearance="big">Вход</Button>
 			</form>
