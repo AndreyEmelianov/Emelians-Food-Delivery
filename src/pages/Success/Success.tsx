ad9dc6512f517b11a371e351decd9816ef09c445
Andrey Emelianov <621077@mail.ru>
2023-09-08 18:54:36 +0300

    реализовал страницу успешного заказа

---
+++ src/pages/Success/Success.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/ui/Button/Button';
+
+import styles from './Success.module.css';
+
+export function Success() {
+	const navigate = useNavigate();
+
+	return (
+		<div className={styles['success']}>
+			<img src="/success.png" alt="Изображение пиццы" />
+			<div className={styles['text']}>Ваш заказ успешно оформлен!</div>
+			<Button appearance="big" onClick={() => navigate('/')}>
+				Сделать новый
+			</Button>
+		</div>
+	);
+}
